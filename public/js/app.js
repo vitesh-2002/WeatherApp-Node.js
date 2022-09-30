92b849cdb1d3b8db84bb59be02d30b81a2f79f00
vitesh <vitesh2002@gmail.com>
2022-09-30 09:38:30 -0400

    Update clientside JavaScript

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,9 @@
 console.log('Clientside java script file is loaded');
 
-
 const weatherForm = document.querySelector('form');
 const searchElement = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
 const messageTwo = document.querySelector('#message-2');
-
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
